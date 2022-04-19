7dd41ebc0aee2a3576808010e360ffe80488cd9e
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-19 16:21:28 -0400

    update

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
