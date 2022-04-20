86561155280c877004cabd1f37cd4e5ba8f3ebb8
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 11:43:51 -0400

    update19

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello \"Mosh\"";
-        System.out.println(message.trim());
+        // c:\Windows\...
+        String message = "c:\\Windows\\...";
         System.out.println(message);
     }
 }
