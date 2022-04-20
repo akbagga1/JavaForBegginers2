1a9ce1a14393661288ea9ddc004348e15e915fb4
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 14:47:45 -0400

    update35-Math.max

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int result = (int)Math.floor(1.1F);
+        int result = Math.max(1, 2);
         System.out.println(result);
     }
 }
