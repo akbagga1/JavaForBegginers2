2415106cb8a6f82b8833042f3073359c55321d78
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 16:28:08 -0400

    update 39-Ask Name and System.out.print(Name:)-Scanner scanner.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Age: ");
-        byte age = scanner.nextByte();
-        System.out.println("You are " + age);
+        System.out.print("Name: ");
+        String Name = scanner.next();
+        System.out.println("You are " + Name);
     }
 }
