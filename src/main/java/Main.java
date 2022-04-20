84b498a5b5e3b28fb8160fe106f0dbdb0ad99861
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 14:57:08 -0400

    update35-Math.random round & int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        double result = Math.round(Math.random() * 100);
+        int result = (int) Math.round(Math.random() * 100);
         System.out.println(result);
     }
 }
