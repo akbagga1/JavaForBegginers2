5780da08e297b8381da0340df2001226cebea361
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 15:40:12 -0400

    update37-format number to percent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(1234567.891);
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String result = percent.format(0.1);
         System.out.println(result);
     }
 }
