24b95737c4e9011059482dac61e8bc6f1150dcbd
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 14:15:06 -0400

    update31

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        short x = 1;
-        int y = x + 2;
+        //implicit casting
+        //byte > short > int > long > float > double
+        double x = 1.1;
+        double y = x + 2;
         System.out.println(y);
     }
 }
