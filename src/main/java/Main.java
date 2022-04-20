d8f3c678aff2ef87b61f6a7c4a836eb6b6f0e901
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 11:30:33 -0400

    update11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
     }
 }
