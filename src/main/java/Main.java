6008f0328b21c4b1bcee30254ef3886cfb88245d
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 15:29:56 -0400

    update36- format currency decimal digit and dollar sign

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 import java.sql.SQLOutput;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int result = (int) (Math.random() * 100);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(1234567.891);
         System.out.println(result);
     }
 }
