4a5ee86a8bbed9fe1bc1b4245189ca563a5ba234
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 14:42:01 -0400

    update35-Math.round

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //implicit casting
-        //byte > short > int > long > float > double
-        String x = "1.1";
-        double y = Double.parseDouble(x) + 2;
-        System.out.println(y);
+        int result = Math.round(1.1F);
+        System.out.println(result);
     }
 }
