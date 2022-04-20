c4cc69576fa38a95392c603bb23bdcdd057fafd8
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 14:07:21 -0400

    update30-order of operation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int x = (10 + 3) * 2;
-        System.out.println(x);
+        short x = 1;
+        int y = x + 2;
+        System.out.println(y);
     }
 }
