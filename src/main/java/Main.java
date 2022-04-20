f6d4b06c73be8eaa101fb51569aa3b985b179043
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 10:12:46 -0400

    update2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,10 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
-
-        //mortCalc();
-        otherThing();
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
     }
-
-    private static void otherThing() {}
-
-
-    private static void mortCalc() {
-            // do work...
-
-    }
-
 }
