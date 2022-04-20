51e1c9e44cf34cc2a408ba8fe97e144e1f2487f7
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 11:21:20 -0400

    update8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello World!";
+        String message = "Hello World" + "!!";
         System.out.println(message);
     }
 }
