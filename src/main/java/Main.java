de64955913e6199fd131d3a00a77fb09d68f0f11
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 11:37:31 -0400

    update16

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.replace("!","*"));
+        System.out.println(message.toLowerCase());
         System.out.println(message);
     }
 }
