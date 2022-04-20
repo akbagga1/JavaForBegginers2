db6cccc24b051536a1561ae340e9896e1ab87793
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-20 11:09:20 -0400

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
+import java.awt.*;
 import java.sql.SQLOutput;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        byte x = 1;
-        byte y = 1;
-        x = 2;
-        System.out.println(y);
+        Point point1 = new Point (1, 1);
+        Point point2 = point1;
+
     }
 }
